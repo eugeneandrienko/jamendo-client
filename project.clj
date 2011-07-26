8f303e09277e48872b30372aa539fca91ebbc146
Eugene <h0rr0rrdrag@gmail.com>
2011-07-26 17:46:22 +0400

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject jamendo-CLient "1.0.0-SNAPSHOT"
+  :description "Command-line client for Jamendo"
+  :url "https://github.com/h0rr0rrdrag0n/jamendo-CLient"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-http "0.1.3"]]
+  ;; don't forget write :main!
+  :dev-dependencies [[swank-clojure "1.3.2"]])
