5aaf4b9f0e3bb75e1411c39e7dcde4e0c980a576
Eugene <h0rr0rrdrag@gmail.com>
2011-07-28 21:28:58 +0400

    Added function to print list of searched albums

--- src/jamendo_CLient/user_interface.clj
+++ src/jamendo_CLient/user_interface.clj
@@ -1,9 +1,17 @@
 (ns jamendo-CLient.user-interface
+  (:use [jamendo-CLient.jamendoapi-wrappers :only [get-paged-albums-apisafe]])
   (:import java.lang.String))
+
+(declare list-of-commands)
 
 (defn hello-msg []
   "Prints the welcome message"
   (println "Welcome to Jamendo client")
+  (list-of-commands)
+  (println))
+
+(defn list-of-commands []
+  "Print list of valid commands"
   (println)
   (println "You can use next commands:")
   (println "search-albums 'keyword'\t Search albums on the key 'keyword'")
@@ -11,36 +19,30 @@
   (println "play-album 'id'")
   (println "play-songs 'id1 id2 ... idN'")
   (println "play-song 'id'")
-  (println "quit")
-  (println))
+  (println "list-commands")
+  (println "quit"))
 
-(defn prompt []
+(defn prompt [prompt-str]
   "Print prompt and return user input as list"
   (defn string-to-list [string]
     (map (fn [x] x) (.split string " ")))
-  (print "=> ")
+  (print prompt-str)
   (flush)
   (string-to-list (read-line)))
 
-;; func - should be lambda function with one parameter - number
-;; of requested page, which calls proper function with necessary
-;; parameters.
-(defn print-paged-list [func]
-  "Prints paged list returned by 'get-paged-*' functions and
-   return user input"
-  (defn- print-paged-list-iter [func num]
-    (. Thread sleep 1000)
+(defn print-search-albums-result [keyword]
+  "Print result of search by 'keyword' on albums"
+  (loop [page 1
+         result (get-paged-albums-apisafe 10 page keyword)]
     (let
-        [requested-list (func num)]
-      (cond
-       (= requested-list '()) (println "End of list")
-       :else (do
-               (println requested-list)
-               (println)
-               (println "Press Enter to continue or enter the command")
-               (let [user-input (prompt)]
-                 (cond
-                  (= user-input "") (print-paged-list-iter func
-                                                             (+ num 1))
-                  :else user-input))))))
-  (print-paged-list-iter func 1))
+        [nextresult (get-paged-albums-apisafe 10 (+ page 1) keyword)]
+      (if (not= result '{nil nil})
+        (do
+          (println result)
+          (println)
+          (println
+           "Enter 'quit' to return to top-level or press Enter to continue")
+          (if (= (first (prompt "> ")) "quit")
+            :quit
+            (recur (+ page 1)
+                   nextresult)))))))
