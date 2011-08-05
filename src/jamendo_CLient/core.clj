55b2634550fd8ecdd4e90e1bf2b9edfa1fd4ec33
Eugene <h0rr0rrdrag@gmail.com>
2011-08-05 18:15:08 +0400

    New function to parse user input

--- src/jamendo_CLient/core.clj
+++ src/jamendo_CLient/core.clj
@@ -1,23 +1,27 @@
 (ns jamendo-CLient.core
   (:gen-class)
-  (:use [jamendo-CLient.jamendoapi-wrappers :only [get-paged-tags
-                                                   get-paged-albums]]
-        [jamendo-CLient.user-interface :only [hello-msg
+  (:require [clojure.contrib.str-utils2 :as sl])
+  (:use [jamendo-CLient.user-interface :only [hello-msg
                                               list-of-commands
-                                              prompt]]))
+                                              prompt
+                                              print-search-albums-result
+                                              list-album]]))
 
 (defn process-user-commands []
   "Process user input and call proper function
    with transfer of command parameters to func"
-  (loop [user-input (prompt)]
+  (loop [user-input (prompt "=> ")]
     (let [cmd (first user-input)]
       (cond
-       (= cmd "search-albums") nil
+       (= cmd "search-albums") (print-search-albums-result
+                                (sl/join " " (next user-input)))
+       (= cmd "list-commands") (list-of-commands)
+       (= cmd "list-album") (list-album (second user-input))
        :else (if (not= cmd "quit")
                (do (println "Unknown command!")
                    (list-of-commands))))
       (if (not= cmd "quit")
-        (recur (prompt))))))
+        (recur (prompt "=> "))))))
 
 (defn -main [& args]
   (hello-msg)
